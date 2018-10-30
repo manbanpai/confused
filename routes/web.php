dafe996cbf21eb37fb275ca5fcc3e92c88ba2993
manbanpai <761846158@qq.com>
2018-10-30 16:20:29 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+/*
+    $link = mysqli_connect('172.17.179.14','root','iloveyou123');
+    mysqli_select_db('confused_db',$link);
+
+    mysqli_query($link,'show tables');*/
+
+    return view('welcome');
+});
